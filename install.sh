a2d93bc50f4f5214f5d25912b3eadc0294efdcba
nao3301 <nao.lupa@gmail.com>
2018-02-10 00:52:59 +0100

    updated the installscript for mac

--- install.sh
+++ install.sh
@@ -1,8 +1,45 @@
-echo "install ruby via brew"
-brew install ruby
-echo "done"
-echo "installing net-ping via gem"
-sudo gem install net-ping
-echo "done"
+#!/bin/bash
 
-echo "still in process"
+echo "install ruby via brew"
+CHECK=0
+if brew install ruby; then
+    echo "done"
+    CHECK=1
+else
+    echo "Error"
+    CHECK=0
+fi
+echo "installing net-ping via gem"
+if sudo gem install net-ping; then
+    echo "done"
+    CHECK=1
+else
+    echo "Error"
+    CHECK=0
+fi
+echo "create redsec alias file"
+if 
+    echo "#!/bin/bash
+
+cd $PWD
+ruby redsec.rb" > redsec; then 
+    echo "done"
+    CHECK=1
+else
+    echo "Error"
+    CHECK=0
+fi
+echo "copy alias to use redsec from erverywhere"
+if sudo cp $PWD/redsec /usr/local/bin/redsec
+sudo chmod +x /usr/local/bin/redsec; then
+    echo "done"
+    CHECK=1
+else
+    echo "Error"
+    CHECK=0
+fi
+if CHECK==1; then
+    echo "installation completed succsessfully"
+else
+    echo "Something went wrong."
+fi
